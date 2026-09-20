0fd18202666cd30d7fad350df9e89733fddb88c2
RisottoMan <lapka.roman@yandex.ru>
2026-09-20 09:13:57 +0300

    add autoupdate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-cd /home/container
+# Wait for the container to fully initialize
+sleep 1
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Auto update
+if [ -z "${AUTO_UPDATE}" ] || [ "${AUTO_UPDATE}" == "1" ]; then
+    echo "Updating SCP: Secret Laboratory..."
+    cd /home/container/.DepotDownloader || exit 1
+
+    if [ "${BRANCH_TAG}" == "" ]; then
+        echo "Using public branch."
+        ./DepotDownloader -app 996560 -depot 996562 -dir /home/container -validate
+    else
+        echo "Using branch: ${BRANCH_TAG}"
+        ./DepotDownloader -app 996560 -depot 996562 -branch "${BRANCH_TAG}" -dir /home/container -validate
+    fi
+    echo "SCP:SL update finished."
+else
+    echo "Not updating game server as auto update was set to 0."
+fi
 
 MODIFIED_STARTUP="eval $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')"
 echo ":/home/container$ ${MODIFIED_STARTUP}"
